c00d7c2806017c7a575b48b06111b5f760ba080f
ljcl79 <ljcl79@gmail.com>
2024-10-05 13:25:55 -0300

    Aula1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ListaTareas from './componentes/ListaTareas'
+
+function App() {
+
+  return (
+    <>
+      <ListaTareas></ListaTareas>
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/ListaTareas.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface IProps { };
+
+const ListaTareas: FC<IProps> = () => {
+    return <div>Este es mi primer componente en Typescript</div>
+};
+
+export default ListaTareas;
